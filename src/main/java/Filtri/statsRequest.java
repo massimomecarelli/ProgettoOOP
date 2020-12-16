98c21b7c4f2ad45122004427da535e1911c3c958
Massimo Mecarelli <s1093260@studenti.univpm.it>
2020-12-16 20:00:33 +0100

    aggiornamento statsRequest e WeatherService

--- src/main/java/Filtri/statsRequest.java
+++ src/main/java/Filtri/statsRequest.java
@@ -3,9 +3,14 @@
  *******************************************************************************/
 package Filtri;
 
-// Start of user code (user defined imports)
+import java.util.Vector;
 
-// End of user code
+import Parser.JsonParser;
+import model.WeatherCollection;
+import Service.*;
+import model.Weather;
+
+
 
 /**
  * Description of statsRequest.
@@ -13,75 +18,39 @@ package Filtri;
  * @author Massimo
  */
 public class statsRequest {
-	/**
-	 * Description of the property .
-	 */
-	public Integer=Integer.valueOf(0);
-
-	/**
-	 * Description of the property weatherCollections.
-	 */
-	public HashSet<WeatherCollection> weatherCollections = new HashSet<WeatherCollection>();
-
-	// Start of user code (user defined attributes for statsRequest)
-
-	// End of user code
+	
 
 	/**
 	 * The constructor.
 	 */
 	public statsRequest() {
-		// Start of user code constructor for statsRequest)
-		super();
-		// End of user code
+		
 	}
 
 	/**
-	 * Description of the method getStats.
-	 * @param nome 
-	 * @param periodo 
+	 * Description of the method getAll.
+	 * @param lat
+	 * @param lon 
+	 * @param cnt 
 	 */
-	public void getStats(String nome, int periodo) {
-		// Start of user code for method getStats
-		// End of user code
+	public Vector<Weather> getAll(String key, double lat, double lon, int cnt) {
+		Vector<Weather> weather = new Vector<Weather>;
+		JsonParser parser = new JsonParser();
+		weather = parser.readFile(lat, lon, cnt);
+		return weather;
 	}
 
 	/**
-	 * Description of the method getStats.
-	 * @param citta 
-	 * @param periodo 
-	 * @param stat 
+	 * Description of the method getDato.
+	 * @param lat 
+	 * @param lon 
+	 * @param cnt 
+	 * @param dato
 	 */
-	public void getStats(String citta, int periodo, String stat) {
-		// Start of user code for method getStats
-		// End of user code
-	}
-
-	// Start of user code (user defined methods for statsRequest)
-
-	// End of user code
-	/**
-	 * Returns .
-	 * @return  
-	 */
-	public Integer get() {
-		return this.;
-	}
-
-	/**
-	 * Sets a value to attribute . 
-	 * @param new 
-	 */
-	public void set(Integer new) {
-	    this. = new;
-	}
-
-	/**
-	 * Returns weatherCollections.
-	 * @return weatherCollections 
-	 */
-	public HashSet<WeatherCollection> getWeatherCollections() {
-		return this.weatherCollections;
+	public void getDato(double lat, double lon, int cnt) {
+		JsonParser parser = new JsonParser();
+		
+		parser.readFile(lat, lon, cnt);
 	}
 
 }
--- src/main/java/Service/WeatherService.java
+++ src/main/java/Service/WeatherService.java
@@ -2,32 +2,37 @@
  * 2020, All rights reserved.
  *******************************************************************************/
 package Service;
-import org.springframework.web.bind.annotation.RestController;
+import model.*;
+import Filtri.*;
 
+import org.springframework.web.bind.annotation.RestController;
 import Parser.JsonParser;
+
+import java.util.Vector;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import model.WeatherCollection;
+
 
 /**
  * Description of WeatherService.
  * 
  * @author Massimo
  */
+
 @RestController
 public class WeatherService {
 	/**
 	 * Description of the property weathers.
 	 */
-	public HashSet<WeatherCollection> weathers = new HashSet<WeatherCollection>();
 
 	
-	public static boolean obj=true;
-	public static double lat;
-	public static double lon;
-	public static int cnt;
+	public Vector<WeatherCollection> weathers = new Vector<WeatherCollection>();
+
+	private static String key="954cc342074e04241297f65762f40232";
+	private static boolean obj=true;
 
 
 	/**
@@ -39,20 +44,52 @@ public class WeatherService {
 	 * Description of the method updateWeather.
 	 */
 	@GetMapping("/weather")
-	public void getWeather(@RequestParam(value="lat") double latitudine, 
-			@RequestParam(value="lon") double longitudine,
+	public Weather getWeather(
+			@RequestParam(value="lat") double lat, 
+			@RequestParam(value="lon") double lon,
 			@RequestParam(value="cnt", defaultValue="1")int cnt) {
-		this.lat=latitudine;
-		this.lon=longitudine;
-		this.cnt=cnt;
 		if (cnt!=1) obj=false; //caso in cui si vogliano ricevere più giorni
 								//quindi si richiede un JSONArray invece di un JSONObject
 		JsonParser parser = new JsonParser(); //creo un JsonParser
+		
 		//leggo dall'API, passando il tipo di Json e l'url al parser
-		parser.readFile(obj,
-				"api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=954cc342074e04241297f65762f40232&cnt="+cnt+"&units=metric&lang=it");
+		parser.readAPI(obj,"http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+
+				"&appid="+key+"&cnt="+cnt+"&units=metric&lang=it");
 		//ricevo in uscita i dati per quella determinata città
-		parser.getWeather();
+		
+		return parser.getWeather();
 	}
-
+	
+	//statistiche riguardo previsioni azzeccate in generale 
+	@GetMapping("/stats")
+	public statsRequest getStats(
+			@RequestParam(value="lat") double lat, 
+			@RequestParam(value="lon") double lon,
+			@RequestParam(value="cnt", defaultValue="1")int cnt) {
+		
+		statsRequest stat= new statsRequest();
+		return stat.getAll(cnt, lat, lon);
+	}
+	
+	//richiedere statistiche di un solo dato specifico
+	@GetMapping("/stats/pressione")
+	public statsRequest getPres(
+			@RequestParam(value="lat") double lat, 
+			@RequestParam(value="lon") double lon,
+			@RequestParam(value="cnt", defaultValue="1")int cnt) {
+	
+		statsRequest stat = new statsRequest();
+		return stat.getPress(cnt, lat, lon);
+	}
+	
+	@GetMapping("/stats/temperature")
+	public statsRequest getTem(
+			@RequestParam(value="lat") double lat, 
+			@RequestParam(value="lon") double lon,
+			@RequestParam(value="cnt", defaultValue="1")int cnt) {
+	
+		statsRequest stat = new statsRequest();
+		return stat.getTemperature(cnt, lat, lon);
+	}
+	
 }
