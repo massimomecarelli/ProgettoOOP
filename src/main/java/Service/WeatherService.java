88e67270315bf80c71d71812c4fc79cce7f53bb4
Massimo Mecarelli <s1093260@studenti.univpm.it>
2020-12-17 19:44:43 +0100

    Update WeatherService

--- src/main/java/Service/WeatherService.java
+++ src/main/java/Service/WeatherService.java
@@ -32,6 +32,7 @@ public class WeatherService {
 	public Vector<WeatherCollection> weathers = new Vector<WeatherCollection>();
 
 	private static String key="954cc342074e04241297f65762f40232";
+	private static boolean obj=true;
 
 
 	/**
@@ -43,50 +44,56 @@ public class WeatherService {
 	 * Description of the method updateWeather.
 	 */
 	@GetMapping("/weather")
-	public Vector<Weather> getWeather(
+	public Weather getWeather(
 			@RequestParam(value="lat") double lat, 
 			@RequestParam(value="lon") double lon,
 			@RequestParam(value="cnt", defaultValue="1")int cnt) {
+		if (cnt!=1) obj=false; //caso in cui si vogliano ricevere più giorni
+								//quindi si richiede un JSONArray invece di un JSONObject
 		JsonParser parser = new JsonParser(); //creo un JsonParser
 		
 		//leggo dall'API, passando il tipo di Json e l'url al parser
-		parser.readAPI("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+
-				       "&appid="+key+"&cnt="+cnt+"&units=metric&lang=it");
-		//parser.readFile(lat, lon);
+		parser.readAPI(obj,"http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+
+				"&appid="+key+"&cnt="+cnt+"&units=metric&lang=it");
 		//ricevo in uscita i dati per quella determinata città
+		
 		return parser.getWeather();
 	}
 	
 	//statistiche riguardo previsioni azzeccate in generale 
 	@GetMapping("/stats")
-	public statsRequest getStats(
+	public StatsRequest getStats(
 			@RequestParam(value="lat") double lat, 
 			@RequestParam(value="lon") double lon,
 			@RequestParam(value="cnt", defaultValue="1")int cnt) {
 		
-		statsRequest stat= new statsRequest();
-		return stat.getAll(cnt, lat, lon);
+		StatsRequest stat= new StatsRequest();
+		return stat.getAll(lat, lon, cnt);
 	}
 	
 	//richiedere statistiche di un solo dato specifico
 	@GetMapping("/stats/pressione")
-	public statsRequest getPres(
+	public StatsRequest getPres(
 			@RequestParam(value="lat") double lat, 
 			@RequestParam(value="lon") double lon,
 			@RequestParam(value="cnt", defaultValue="1")int cnt) {
 	
-		statsRequest stat = new statsRequest();
-		return stat.getPress(cnt, lat, lon);
+		StatsRequest stat = new StatsRequest();
+		return stat.getPress(lat, lon, cnt);
 	}
 	
 	@GetMapping("/stats/temperature")
-	public statsRequest getTem(
+	public StatsRequest getTem(
 			@RequestParam(value="lat") double lat, 
 			@RequestParam(value="lon") double lon,
-			@RequestParam(value="cnt", defaultValue="1")int cnt) {
-	
-		statsRequest stat = new statsRequest();
-		return stat.getTemperature(cnt, lat, lon);
+			@RequestParam(value="cnt", defaultValue="giornaliero")String cnt) {
+		StatsRequest stat = new StatsRequest();
+		if (cnt.equals("giornaliero"))
+			return stat.getTemperature(lat, lon, 1);
+		else if (cnt.equals("settimanale"))
+			return stat.getTemperature(lat, lon, 7);
+		else if (cnt.equals("mensile"))
+			return stat.getTemperature(lat, lon, 30);
 	}
 	
 }
