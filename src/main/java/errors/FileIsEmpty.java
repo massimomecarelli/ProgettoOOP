bdc805a2f710292e3d55671768882b1b10a9ef8a
Massimo Mecarelli <s1093260@studenti.univpm.it>
2021-01-11 15:40:44 +0100

    eccezione FileIsEmpty

--- src/main/java/errors/FileIsEmpty.java
+++ src/main/java/errors/FileIsEmpty.java
@@ -1,5 +1,28 @@
 package errors;
 
-public class FileIsEmpty {
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class FileIsEmpty extends Exception{
+	
+	public FileIsEmpty(Exception e,HttpServletResponse response) {
+		resolveException(e,response);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@ExceptionHandler(value=FileNotFound.class)
+	public void resolveException(Exception ex,HttpServletResponse response) {
+		try {
+			response.getOutputStream().println("Warning: File vuoto\nException message: "+ex.toString());
+		} catch (IOException e) {
+			
+		}
+	}
+
+	
 }
